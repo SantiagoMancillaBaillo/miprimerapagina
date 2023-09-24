91ca91b9412d4837fa68039dd71ef31d17356507
SantiagoMancillaBaillo <santibaillo@hotmail.com>
2023-09-24 20:35:05 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+//alert("Hola este es mi Javascript");
+
+let menu_responsive = document.querySelector(".checkbtn");
+
+menu_responsive.onclick = function () {
+    navBar = document.querySelector(".navbar");
+    navBar.classList.toggle("active");
+};
+
+
+const form = document.getElementById("form");
+const nombre = document.getElementById("nombre");
+const parrafo = document.getElementById("alertas");
+
+function validarFormulario() {
+  let warnings = "";
+  let valido = true;
+  parrafo.innerHTML = "";
+
+  if (nombre.value.length < 4) {
+    warnings += `¡ERROR!  El nombre debe contener más de 4 carcateres`;
+    valido = false;
+  }
+
+  if (!valido) {
+    parrafo.innerHTML = warnings;
+  } else {
+    parrafo.innerHTML = "Enviado";
+    alert("¡Enviado con Éxito!")
+  }
+  return valido;
+}
+
+form.addEventListener("submit", (e) => {
+  if (validarFormulario()) {
+    // Si la validación es exitosa, puedes enviar el formulario
+    formulario.submit();
+  } else {
+    e.preventDefault(); // Evita que el formulario se envíe automáticamente
+  }
+});
